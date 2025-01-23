61754b271c5e48b1ae49e18a80588cc9eff8ec76
aretyc <szymon1621997@gmail.com>
2025-01-23 18:49:59 +0100

    move app source to src  directory

---
+++ src/templates_c/%%D7^D71^D71D8451%%motd.tpl.php
@@ -0,0 +1,4 @@
+<?php /* Smarty version 2.6.31, created on 2025-01-23 17:44:36
+         compiled from motd.tpl */ ?>
+Hello, <?php echo $this->_tpl_vars['username']; ?>
+! Welcome to TUDO -admin :)
